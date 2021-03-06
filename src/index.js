fbc111f7ff17f9414863895119ae9f18272816c5
Svitlana Grytsai <34807666+SvitlanaG@users.noreply.github.com>
2021-03-06 17:10:43 +0100

    feat: solve task brackets

--- src/index.js
+++ src/index.js
@@ -1,3 +1,17 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
-}
+    let newArr = str.split("");
+    let newArrLength = newArr.length;
+    let stackArr = [];
+      for (let i = 0; i < newArrLength; i++) {
+        stackArr.push(newArr[i]);
+          if (stackArr.length > 1) {
+              let sample = stackArr.slice(-2)
+                  for (let j = 0; j < bracketsConfig.length; j++) {
+                      if (JSON.stringify(sample) === JSON.stringify(bracketsConfig[j])) {
+                          stackArr.splice(-2);
+                      }
+                  }
+          }
+      }
+    return stackArr.length === 0 ? true : false;
+  }
